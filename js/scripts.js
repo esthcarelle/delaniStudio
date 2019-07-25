c4e3d950c4ce253cb8a827d2bef09d7e3948aef6
esthcarelle <esthcarelle@gmail.com>
2019-07-25 14:51:18 +0200

    add images in portofolio

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+$("#show").click(function(){
+$("#show").hide();
+$("#hide").show();
+});
+$("#hide").click(function(){
+    $("#show").show();
+    $("#hide").hide();
+    });
+    $("#show1").click(function(){
+        $("#show1").hide();
+        $("#hide1").show();
+        });
+        $("#hide1").click(function(){
+            $("#show1").show();
+            $("#hide1").hide();
+            });
+            
+            $("#show2").click(function(){
+                $("#show2").hide();
+                $("#hide2").show();
+                });
+                $("#hide2").click(function(){
+                    $("#show2").show();
+                    $("#hide2").hide();
+                    });
+                    
+});

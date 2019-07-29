3ef11b796c59c1e34530d36aca02496dc8bac6ea
esthcarelle <esthcarelle@gmail.com>
2019-07-29 09:07:46 +0200

    correct indentation for scripts.js

---
+++ README.md
@@ -0,0 +1,27 @@
+#Delani-studio
+### by **Esther Carrelle**
+##DESCRIPTION
+##studio
+It is a website that shows you the work of Delani studio,has a shord description about it,what they do,some pictures of  projects they have done and a contact form through where a user can send a message.
+#SETUP INSTALLATION REQUIRED
+* Google chrome
+* or firefox
+* or opera
+##TECHNOLOGY USED
+* HTML
+* CSS
+* BOOTSTRAP
+* JAVASCRIPT
+#BDD
+* read each section of the website
+* input your name in the contact section
+* input your email after your name
+* input the message you want to send to us
+* click submit
+
+## MY PROJECT LINK
+
+## SUPPORT AND CONTACT
+email:esthcarelle@gmail.com
+##LICENCE
+Licenced by MIT copyright @2019 **Esther Carrelle**
--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,52 @@
 $(document).ready(function(){
-$("#show").click(function(){
-$("#show").hide();
-$("#hide").show();
-});
-$("#hide").click(function(){
-    $("#show").show();
-    $("#hide").hide();
+    $("#show").click(function(){
+        $("#show").hide();
+        $("#hide").show();
+    });
+    $("#hide").click(function(){
+        $("#show").show();
+        $("#hide").hide();
     });
     $("#show1").click(function(){
         $("#show1").hide();
         $("#hide1").show();
-        });
-        $("#hide1").click(function(){
-            $("#show1").show();
-            $("#hide1").hide();
-            });
+    });
+    $("#hide1").click(function(){
+        $("#show1").show();
+        $("#hide1").hide();
+    });
             
-            $("#show2").click(function(){
-                $("#show2").hide();
-                $("#hide2").show();
-                });
-                $("#hide2").click(function(){
-                    $("#show2").show();
-                    $("#hide2").hide();
-                    });
-                    $("#blanks").submit(function(event){
-                        var email=$("input#email").val();
-                        var name=$("input#name").val();
-                        var message= $.trim($("#message").val());
-                        if((name!="")&&(email!="")&&(message!="")){
-                            alert(name + " we have received your message. Thank you fro reaching out to us.")
-                        }
-                        else{
-                            alert("Fill nicely the form please!");
-                        }
-                        event.preventDefault();
-;
-                    });
-                    var element=["img11","img12","img13","img14","img15","img16","img17","img18"];
-                    element.forEach(function(blank){
-                        $("#"+blank).mouseout(function(){
-                            $("#"+blank+" >h1").hide();
+    $("#show2").click(function(){
+        $("#show2").hide();
+        $("#hide2").show();
+    });
+    $("#hide2").click(function(){
+        $("#show2").show();
+        $("#hide2").hide();
+    });
+    $("#blanks").submit(function(event){
+        var email=$("input#email").val();
+        var name=$("input#name").val();
+        var message= $.trim($("#message").val());
+        if((name!="")&&(email!="")&&(message!="")){
+                alert(name + " we have received your message. Thank you fro reaching out to us.")
+        }
+        else{
+                alert("Fill nicely the form please!");
+        }
+        event.preventDefault();
 
-                        });
-                        $("#"+blank).mouseover(function(){
-                            $("#"+blank+" >h1").show();
+    });
+    var element=["img11","img12","img13","img14","img15","img16","img17","img18"];
+    element.forEach(function(blank){
+            $("#"+blank).mouseout(function(){
+                    $("#"+blank+" >h1").hide();
+                    $("#"+blank+" p").hide();
 
-                        });
-
-                    });
-
-                    
+            });
+            $("#"+blank).mouseover(function(){
+                    $("#"+blank+" >h1").show();
+                    $("#"+blank+" >p").show();
+            });
+});                  
 });
